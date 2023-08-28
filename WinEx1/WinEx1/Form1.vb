5550c73e24c9212e7a03f6bb0dd138e82b0c2d29
luwickX3 <124098586+luwickX3@users.noreply.github.com>
2023-08-28 12:18:03 -0600

    Add files via upload

---
+++ WinEx1/WinEx1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.Office.Interop.Excel
+Imports Microsoft.Office.Interop.Word
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Dialogo = New SaveFileDialog()
+        If Dialogo.ShowDialog() <> DialogResult.OK Then
+            End
+        End If
+        Dim Ruta = Dialogo.FileName
+        Dim WordApp = New Microsoft.Office.Interop.Word.Application
+        Dim WordDoc = WordApp.Documents.Add()
+        WordApp.Selection.TypeText(TextBox1.Text)
+        WordApp.ActiveDocument.SaveAs2(Ruta)
+        WordApp.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Dialogo = New SaveFileDialog()
+        If Dialogo.ShowDialog() <> DialogResult.OK Then
+            End
+        End If
+        Dim Ruta = Dialogo.FileName
+        Dim ExcelApp = New Microsoft.Office.Interop.Excel.Application
+        Dim ExcelBook = ExcelApp.Workbooks.Add()
+        ExcelBook.Sheets(1).Cells(1, 1) = TextBox1.Text
+        ExcelBook.SaveAs(Ruta)
+        ExcelApp.Visible = True
+    End Sub
+End Class
